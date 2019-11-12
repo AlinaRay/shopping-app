243ecfd4ca21aba29065448831ddbb86b27310cb
Alina Ray <a.alina.ray@gmail.com>
2019-11-12 15:40:32 +0200

    cart & orders were implemented

--- App.js
+++ App.js
@@ -4,12 +4,17 @@ import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
 import {AppLoading} from 'expo';
 import * as Font from 'expo-font';
+// import {composeWithDevTools} from 'redux-devtools-extension';
 
 import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
+import ordersReducer from './store/reducers/orders';
 import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
     products: productsReducer,
+    cart: cartReducer,
+    orders: ordersReducer
 });
 
 const store = createStore(rootReducer);
@@ -17,7 +22,7 @@ const store = createStore(rootReducer);
 const fetchFonts = () => {
     return Font.loadAsync({
         'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-        'open-sans-bold': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
     });
 };
 
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {FlatList} from 'react-native';
-import {useSelector} from 'react-redux';
+import {FlatList, Platform} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
 
+import HeaderButton from '../../components/UI/HeaderButton';
 import ProductItem from '../../components/shop/ProductItem';
+import * as cartActions from '../../store/actions/cart';
 
 const ProductOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts);
+    const dispatch = useDispatch();
+
     return (
         <FlatList
             data={products}
@@ -17,11 +22,12 @@ const ProductOverviewScreen = props => {
                     title={itemData.item.title}
                     onViewDetail={() => {
                         props.navigation.navigate('ProductDetail', {
-                                productId: itemData.item.id,
-                                productTitle: itemData.item.title,
-                            });
+                            productId: itemData.item.id,
+                            productTitle: itemData.item.title,
+                        });
                     }}
                     onAddToCart={() => {
+                        dispatch(cartActions.addToCart(itemData.item));
                     }}
                 />
             }
@@ -29,8 +35,33 @@ const ProductOverviewScreen = props => {
     )
 };
 
-ProductOverviewScreen.navigationOptions = {
-    headerTitle: 'All Products',
+ProductOverviewScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'All Products',
+        headerLeft: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title="Menu"
+                    iconName={Platform.OS === 'andoroid' ? 'md-menu' : 'ios-menu'}
+                    onPress={() => {
+                        navData.navigation.toggleDrawer()
+                    }}
+                />
+            </HeaderButtons>
+        ),
+        headerRight: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title="Cart"
+                    iconName={Platform.OS === 'andoroid' ? 'md-cart' : 'ios-cart'}
+                    onPress={() => {
+                        navData.navigation.navigate('Cart')
+                    }}
+                />
+            </HeaderButtons>
+        )
+    }
+
 };
 
 export default ProductOverviewScreen;
