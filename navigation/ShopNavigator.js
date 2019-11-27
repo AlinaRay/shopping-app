512979dd95c2c2cfde45bacece81bf1e24b7b77d
Alina Ray <a.alina.ray@gmail.com>
2019-11-27 13:28:22 +0200

    logout was implemented

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {createStackNavigator} from 'react-navigation-stack';
-import {createDrawerNavigator} from 'react-navigation-drawer';
+import {createDrawerNavigator, DrawerNavigatorItems} from 'react-navigation-drawer';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-import {Platform} from 'react-native';
+import {Platform, SafeAreaView, Button, View} from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
+import {useDispatch} from 'react-redux';
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
@@ -14,6 +15,7 @@ import EditProductScreen from '../screens/user/EditProductScreen';
 import AuthScreen from '../screens/user/AuthScreen';
 import StartupScreen from '../screens/StartupScreen';
 import Colors from '../constants/Colors';
+import * as authActions from '../store/actions/auth';
 
 const defaultNavOptions = {
     headerStyle: {
@@ -84,6 +86,25 @@ const ShopNavigator = createDrawerNavigator({
 }, {
     contentOptions: {
         activeTintColor: Colors.primary
+    },
+    contentComponent: props => {
+        const dispatch = useDispatch();
+        // DrawerNavigatorItems is used for added all sidebar items
+        return (
+            <View style={{flex: 1, padding: 20}}>
+                <SafeAreaView forceInset={{top: 'always', horizontal: 'never'}}>
+                    <DrawerNavigatorItems {...props} />
+                    <Button
+                        title="Logout"
+                        color={Colors.primary}
+                        onPress={() => {
+                            dispatch(authActions.logout());
+                            props.navigation.navigate('Auth');
+                        }}
+                    />
+                </SafeAreaView>
+            </View>
+        )
     }
 });
 
